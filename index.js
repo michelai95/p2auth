fba8a60630c203ba909a86c87a17cb0579c20080
michela iacobucci <michelaiacobucci@MacBook-Air.local>
2020-06-29 11:52:45 -0700

    added middleware

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
+// required NPM libraries
 // configure dotenv
 require('dotenv').config()
 // require express and setup express app instance 
 const Express = require('express')
 // require and set view engine using ejs
 const ejsLayouts = require('express-ejs-layouts')
-// set app to use false URL encoding 
-// set app public directory for use 
-// set app ejs layouts for render 
+// require all middleware for app/authentication 
+// helmet, morgan, passport, and custom middleware for people being logged in long term
+const helmet = require('helmet')
+// express sessions/sequelize sessions 
+const session = require('express-session')
+const flash = require('flash')
 
+// app setup
 const app = Express()
+// set app to use false URL encoding 
 app.use(Express.urlencoded({ extended: false}))
+// set app public directory for use 
 app.use(Express.static(__dirname + '/public'))
 app.set('view engine', 'ejs')
+// set app ejs layouts for render 
 app.use(ejsLayouts)
+app.use(require('morgan')('dev'))
+app.use(helmet())
+
 
 app.get('/', (req, res) => {
     // check to see if user has logged in
